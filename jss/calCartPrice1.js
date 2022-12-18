3716fa00f08ae961ab2a86d125a734896c4ce385
Yerbol <210103396@stu.sdu.edu.kz>
2022-12-18 17:49:06 +0600

    coffeeboom

---
+++ jss/calCartPrice1.js
@@ -0,0 +1,28 @@
+function calCartPrice() {
+    const cartItems = document.querySelectorAll('.product-port');
+    const totalPriceEl = document.querySelector('.total-price');
+    const deliveryCost = document.querySelector('.delivery-cost');
+    const cartDelivery = document.querySelector('[data-card-delivery]');
+
+    let totalPrice = 0;
+
+    cartItems.forEach(function (item) {
+        const amountEl = item.querySelector('[data-counter]');
+        const priceEl = item.querySelector('.price__currency');
+        const currentPrice = parseInt(amountEl.innerText) * parseInt(priceEl.innerText);
+        totalPrice += currentPrice ;
+
+    })
+
+    totalPriceEl.innerText = totalPrice - 27000;
+
+    if(totalPrice >=31500){
+        deliveryCost.innerText = 'бесплатно';
+    }else {
+        deliveryCost.innerText = '2000 tg';
+        totalPriceEl.innerText = totalPrice - 25000;
+    }
+
+
+    
+}
